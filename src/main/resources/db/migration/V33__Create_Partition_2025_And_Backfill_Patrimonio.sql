3298916f185c30890fc12715e594344f5b619253
Joao Victor <joaovictooroc@gmail.com>
2026-05-07 00:39:00 -0300

    feat: create 2025 partition and backfill historical patrimonio data

---
+++ src/main/resources/db/migration/V33__Create_Partition_2025_And_Backfill_Patrimonio.sql
@@ -0,0 +1,28 @@
+-- V33: Criação da partição 2025 e Backfill de dados
+-- Removemos o timeout da sessão do Flyway para evitar cancelamentos durante o deploy
+SET statement_timeout = 0;
+
+-- 1. Cria a partição para 2025
+CREATE TABLE IF NOT EXISTS patrimonio_historico_2025 PARTITION OF patrimonio_historico
+    FOR VALUES FROM ('2025-01-01') TO ('2026-01-01');
+
+-- 2. Backfill: Maio 2026 (mês atual)
+INSERT INTO patrimonio_historico (id, usuario_id, data_referencia, valor_total, saldo_contas, total_investido)
+SELECT nextval('patrimonio_historico_id_seq'), u.id, d::DATE, 0, 0, 0
+FROM usuarios u
+CROSS JOIN generate_series('2026-05-01'::DATE, '2026-05-07'::DATE, INTERVAL '1 day') d
+WHERE u.ativo = true AND u.email_verificado = true;
+
+-- 3. Backfill: Abril 2026
+INSERT INTO patrimonio_historico (id, usuario_id, data_referencia, valor_total, saldo_contas, total_investido)
+SELECT nextval('patrimonio_historico_id_seq'), u.id, d::DATE, 0, 0, 0
+FROM usuarios u
+CROSS JOIN generate_series('2026-04-01'::DATE, '2026-04-30'::DATE, INTERVAL '1 day') d
+WHERE u.ativo = true AND u.email_verificado = true;
+
+-- 4. Backfill: Março 2026
+INSERT INTO patrimonio_historico (id, usuario_id, data_referencia, valor_total, saldo_contas, total_investido)
+SELECT nextval('patrimonio_historico_id_seq'), u.id, d::DATE, 0, 0, 0
+FROM usuarios u
+CROSS JOIN generate_series('2026-03-01'::DATE, '2026-03-31'::DATE, INTERVAL '1 day') d
+WHERE u.ativo = true AND u.email_verificado = true;
